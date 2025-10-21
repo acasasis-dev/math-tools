2030ecd1264965c3ec2025bf7b1a54bf7cfd43f9
acasasis-dev <angelito.casasis@gmail.com>
2025-10-21 23:58:31 +0800

    Points mean solution latex

--- math_tools/solution_generator/statistics/covariance.py
+++ math_tools/solution_generator/statistics/covariance.py
@@ -1,13 +1,26 @@
-def covariance(x, y, labels=None):
-	_labels = ("x", "y")
+from statistics import mean
+
+def points_mean_solution_latex(points, points_label):
+	points_mean = mean(points)
+	points_stringified = " + ".join(list(map(str, points)))
+	points_len = len(points)
+	return (
+		f"\t\\sigma_{{\\text{{{points_label}}}}} = \\frac{{{points_stringified}}}{{{points_len}}} = \\frac{{{sum(points)}}}{{{points_len}}} = {points_mean} \\\\\n"
+	)
+
+def covariance(x, y, labels=("x", "y")):
+	output = "\\begin{gather*}\n"
 	if len(x) != len(y):
 		raise Exception(f"length of x must be the same as y. {len(x)} != {len(y)}")
 	
 	if labels:
 		if len(labels) != 2:
 			raise Exception(f"length of labels must exactly be 2")
-	else:
-		_labels = labels
 
-	# TODO: Implement covariance computation and generate latex here
+	x_label, y_label = labels
 	
+	output += points_mean_solution_latex(x, x_label)
+	output += points_mean_solution_latex(y, y_label)
+
+	output += "\\end{gather*}"
+	return output
