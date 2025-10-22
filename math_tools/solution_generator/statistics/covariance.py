45da0f868d92b9aa580e734ec4614a136706c103
acasasis-dev <angelito.casasis@gmail.com>
2025-10-22 11:55:27 +0800

    Cleaned up newlines

--- math_tools/solution_generator/statistics/covariance.py
+++ math_tools/solution_generator/statistics/covariance.py
@@ -1,11 +1,13 @@
 from statistics import mean
 
+new_line = "\\\\ \n"
+
 def points_mean_solution_latex(points, points_label):
 	points_mean = mean(points)
 	points_stringified = " + ".join(list(map(str, points)))
 	points_len = len(points)
 	return (
-		f"\t\\sigma_{{\\text{{{points_label}}}}} = \\frac{{{points_stringified}}}{{{points_len}}} = \\frac{{{sum(points)}}}{{{points_len}}} = {points_mean} \\\\\n"
+		f"\t\\sigma_{{\\text{{{points_label}}}}} = \\frac{{{points_stringified}}}{{{points_len}}} = \\frac{{{sum(points)}}}{{{points_len}}} = {points_mean} {new_line}"
 	), points_mean
 
 def covariance(x, y, labels=("x", "y")):
@@ -22,7 +24,7 @@ def covariance(x, y, labels=("x", "y")):
 	output += x_latex
 	y_latex, y_mean = points_mean_solution_latex(y, y_label)
 	output += y_latex
-	output += "\t\\\\\n"
+	output += f"\t{new_line}"
 	cov_prefix = f"\tCov[\\text{{{x_label}}}, \\text{{{y_label}}}] = "
 	output += cov_prefix
 	cov_numerator = []
@@ -31,11 +33,11 @@ def covariance(x, y, labels=("x", "y")):
 		cov_numerator.append(f"({round(x[i] - x_mean, 2)})({round(y[i] - y_mean, 2)})")
 		cov_numerator_products.append(round((x[i] - x_mean) * (y[i] - y_mean), 2))
 	
-	output += f"\\frac{{{" + ".join(cov_numerator)}}}{{{len(x)} - 1}} \\\\ \n"
+	output += f"\\frac{{{" + ".join(cov_numerator)}}}{{{len(x)} - 1}} {new_line}"
 	output += cov_prefix
-	output += f"\\frac{{{" + ".join(list(map(str, cov_numerator_products)))}}}{{{len(x) - 1}}} \\\\ \n"
+	output += f"\\frac{{{" + ".join(list(map(str, cov_numerator_products)))}}}{{{len(x) - 1}}} {new_line}"
 	output += cov_prefix
-	output += f"\\frac{{{round(sum(cov_numerator_products), 2)}}}{{{len(x) - 1}}} \\\\ \n"
+	output += f"\\frac{{{round(sum(cov_numerator_products), 2)}}}{{{len(x) - 1}}} {new_line}"
 	output += cov_prefix
 	cov = round(sum(cov_numerator_products) / (len(x) - 1), 2)
 	output += f"{cov} \n"
