9318007288082d4a433e1a80bdf8d59c96ddac93
acasasis-dev <angelito.casasis@gmail.com>
2025-10-25 13:09:08 +0800

    Fixed mean prefix

--- math_tools/solution_generator/statistics/lib/mean.py
+++ math_tools/solution_generator/statistics/lib/mean.py
@@ -1,5 +1,5 @@
 from statistics import mean as m
-from math_tools.tools.latex import bar, frac, new_line
+from math_tools.tools.latex import bar, frac, new_line, mu, x_bar
 
 
 def mean(data, label=None, population="full", tabs=1):
@@ -9,5 +9,5 @@ def mean(data, label=None, population="full", tabs=1):
 	points_mean = m(data)
 	points_stringified = " + ".join(list(map(str, data)))
 	points_len = len(data)
-	prefix = "\\mu" if population == "full" else bar(label)
+	prefix = mu(label) if population == "full" else x_bar
 	return f"{'\t' * tabs}{prefix} = {frac(points_stringified, points_len)} = {frac(sum(data), points_len)} = {points_mean} {new_line()}", points_mean
