ff228cd0fb03fde3ad01d634e8f5544b03c6bca7
acasasis-dev <angelito.casasis@gmail.com>
2025-10-25 13:26:09 +0800

    Round mean result to 2 decimal places

--- math_tools/solution_generator/statistics/lib/mean.py
+++ math_tools/solution_generator/statistics/lib/mean.py
@@ -6,7 +6,7 @@ def mean(data, label=None, population="full", tabs=1):
 	if population not in ["full", "sample"]:
 		raise Exception("Invalid population")
 
-	points_mean = m(data)
+	points_mean = round(m(data), 2)
 	points_stringified = " + ".join(list(map(str, data)))
 	points_len = len(data)
 	prefix = mu(label) if population == "full" else x_bar(label)
