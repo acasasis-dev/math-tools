e3a2e4c2d8c76ab4796a5eb35f195dcccc24ad80
acasasis-dev <angelito.casasis@gmail.com>
2025-10-25 13:10:14 +0800

    Added forgot to include label for x_bar

--- math_tools/solution_generator/statistics/lib/mean.py
+++ math_tools/solution_generator/statistics/lib/mean.py
@@ -1,5 +1,5 @@
 from statistics import mean as m
-from math_tools.tools.latex import bar, frac, new_line, mu, x_bar
+from math_tools.tools.latex import frac, new_line, mu, x_bar
 
 
 def mean(data, label=None, population="full", tabs=1):
@@ -9,5 +9,5 @@ def mean(data, label=None, population="full", tabs=1):
 	points_mean = m(data)
 	points_stringified = " + ".join(list(map(str, data)))
 	points_len = len(data)
-	prefix = mu(label) if population == "full" else x_bar
+	prefix = mu(label) if population == "full" else x_bar(label)
 	return f"{'\t' * tabs}{prefix} = {frac(points_stringified, points_len)} = {frac(sum(data), points_len)} = {points_mean} {new_line()}", points_mean
