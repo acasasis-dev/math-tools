14634b3aa416c5ad5d1abd8a1b107d4dfd624268
acasasis-dev <angelito.casasis@gmail.com>
2025-10-26 17:50:13 +0800

    Added standard deviation in solution_generator.statistics direct import

--- math_tools/solution_generator/statistics/__init__.py
+++ math_tools/solution_generator/statistics/__init__.py
@@ -1,6 +1,7 @@
 from .lib.covariance import covariance as cov
 from .lib.mean import mean as m
 from .lib.variance import variance as v
+from .lib.standard_deviation import standard_deviation as sd
 
 
 def covariance(*args, **kwargs):
@@ -13,3 +14,7 @@ def mean(*args, **kwargs):
 
 def variance(*args, **kwargs):
 	return v(*args, **kwargs)
+
+
+def standard_deviation(*args, **kwargs):
+	return sd(*args, **kwargs)
