defeff65b6cbcf1e5eedf44701009e190fd82400
acasasis-dev <angelito.casasis@gmail.com>
2025-10-26 18:25:06 +0800

    Made the usage of mean customizable whether to include env or not

--- math_tools/solution_generator/statistics/lib/mean.py
+++ math_tools/solution_generator/statistics/lib/mean.py
@@ -2,7 +2,7 @@ from statistics import mean as m
 from math_tools.tools.latex import frac, new_line, mu, x_bar
 
 
-def mean(data, label=None, population="full", tabs=1):
+def mean(data, label=None, population="full", tabs=1, environment=True):
 	if population not in ["full", "sample"]:
 		raise Exception("Invalid population")
 
@@ -10,4 +10,12 @@ def mean(data, label=None, population="full", tabs=1):
 	points_stringified = " + ".join(list(map(str, data)))
 	points_len = len(data)
 	prefix = mu(label) if population == "full" else x_bar(label)
-	return f"{'\t' * tabs}{prefix} = {frac(points_stringified, points_len)} = {frac(sum(data), points_len)} = {points_mean} {new_line()}", points_mean
+	output = f"{'\t' * tabs}{prefix} = {frac(points_stringified, points_len)} = {frac(sum(data), points_len)} = {points_mean} {new_line()}"
+	if environment:
+		output = (
+			"\\begin{gather*} \n"
+			f"{output}"
+			"\\end{gather*}"
+		)
+
+	return output
