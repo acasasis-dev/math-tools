5d7a6f5f5d2000512680de28723163ca270e9db9
acasasis-dev <angelito.casasis@gmail.com>
2025-10-26 16:52:00 +0800

    Added the mean solution in the variance output

--- math_tools/solution_generator/statistics/lib/variance.py
+++ math_tools/solution_generator/statistics/lib/variance.py
@@ -1,2 +1,12 @@
-def variance():
-	return ""
+from .mean import mean
+from math_tools.tools.latex import new_line
+
+
+def variance(data, label=None, population="full", tabs=1):
+	kwargs = locals()
+	output = "\\begin{gather*}\n"
+	_mean_latex, _mean = mean(**kwargs)
+	output += _mean_latex
+	output += "\\end{gather*}"
+	
+	return output
