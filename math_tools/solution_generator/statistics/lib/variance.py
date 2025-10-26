776f683a08398ebd6b8d165811a15b353fd55110
acasasis-dev <angelito.casasis@gmail.com>
2025-10-26 17:10:50 +0800

    Added the latex for the first to last step of variance solution

--- math_tools/solution_generator/statistics/lib/variance.py
+++ math_tools/solution_generator/statistics/lib/variance.py
@@ -8,6 +8,14 @@ def variance(data, label=None, population="full", tabs=1):
 	_mean_latex, _mean = mean(**kwargs)
 	output += _mean_latex
 	prefix = f"{sigma() if population == "full" else "s^2"} = "
+	numerator_first_step = []
+	numerator_second_step = []
+	squared_deviations = []
+	for num in data:
+		numerator_first_step.append(f"({num} - {_mean})^2")
+		numerator_second_step.append(f"({round(num - _mean, 2)})^2")
+		squared_deviations.append(round((num - _mean)**2, 2))
+
 	output += "\\end{gather*}"
 
 	return output
