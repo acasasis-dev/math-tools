29b64db9140b0ef0303aed577b8dc52a9a0af0b3
acasasis-dev <angelito.casasis@gmail.com>
2025-11-01 05:16:46 +0800

    Init latex method of correlation

--- math_tools/solution_generator/statistics/correlation.py
+++ math_tools/solution_generator/statistics/correlation.py
@@ -1,20 +1,13 @@
 from math_tools.tools.latex import frac, text, new_line
 from .lib import StatisticsEquation
 from .mean import Mean
-from .variance import Variance
+from .covariance import Covariance
 
 
 class Correlation(StatisticsEquation):
-	def __init__(self, data, label=("x", "y"), population="full", tabs=1, environment=True):
+	def __init__(self, data=[], label=("x", "y"), population="full", tabs=1, environment=True, covariance=None, x_sd=None, y_sd=None):
 		super().__init__(data, label, population, tabs, environment)
-		if len(self.data) != 2:
-			raise Exception(f"length of data must exactly be 2")
-		
-		self.x, self.y = data
-		if len(self.x) != len(self.y):
-			raise Exception(f"length of x must be the same as y. {len(self.x)} != {len(self.y)}")
-		
-		if len(self.label) != 2:
-			raise Exception(f"length of labels must exactly be 2")
-		
-		self.x_label, self.y_label = self.label
+	
+	@property
+	def latex(self):
+		pass
