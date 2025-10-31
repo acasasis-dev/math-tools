d75c683bbc254604073d9c36cc08734fc546016d
acasasis-dev <angelito.casasis@gmail.com>
2025-11-01 05:19:53 +0800

    Implemented fetching of covariance of correlation

--- math_tools/solution_generator/statistics/correlation.py
+++ math_tools/solution_generator/statistics/correlation.py
@@ -7,6 +7,28 @@ from .covariance import Covariance
 class Correlation(StatisticsEquation):
 	def __init__(self, data=[], label=("x", "y"), population="full", tabs=1, environment=True, covariance=None, x_sd=None, y_sd=None):
 		super().__init__(data, label, population, tabs, environment)
+		if not covariance:
+			if len(self.data) != 2:
+				raise Exception(f"length of data must exactly be 2")
+			
+			self.x, self.y = data
+			if len(self.x) != len(self.y):
+				raise Exception(f"length of x must be the same as y. {len(self.x)} != {len(self.y)}")
+			
+			if len(self.label) != 2:
+				raise Exception(f"length of labels must exactly be 2")
+			
+			self.x_label, self.y_label = self.label
+			_covariance = Covariance(
+				self.data,
+				self.label,
+				self.population,
+				self.tabs,
+				self.environment
+			)
+			self.covariance = _covariance.result
+		else:
+			self.covariance = covariance
 	
 	@property
 	def result(self):
