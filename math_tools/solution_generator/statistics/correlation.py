790db26316d6e2aaba7cd9ad4fbe117e830868f4
acasasis-dev <angelito.casasis@gmail.com>
2025-10-31 18:55:48 +0800

    Init correlation init method

--- math_tools/solution_generator/statistics/correlation.py
+++ math_tools/solution_generator/statistics/correlation.py
@@ -1,7 +1,9 @@
 from math_tools.tools.latex import frac, text, new_line
-from .lib import Equation
+from .lib import StatisticsEquation
 from .mean import Mean
 
 
-class Correlation(Equation):
-	pass
+class Correlation(StatisticsEquation):
+	def __init__(self, data, label=("x", "y"), population="full", tabs=1, environment=True):
+		super().__init__(data, label, population, tabs, environment)
+		
