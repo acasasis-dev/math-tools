2a5a4dfad85ea5f02525a3488582acfdb35d6b80
acasasis-dev <angelito.casasis@gmail.com>
2025-11-02 17:19:36 +0800

    Rendered result of grouped data sd

--- math_tools/solution_generator/statistics/grouped_data_sd.py
+++ math_tools/solution_generator/statistics/grouped_data_sd.py
@@ -2,12 +2,13 @@ from .lib import StatisticsEquation
 from .grouped_data_variance import GroupedDataVariance
 
 from math import sqrt
+from math_tools.tools.latex import new_line, sqrt as s
 
 
 class GroupedDataSD(StatisticsEquation):
 	def __init__(self, data, label=("x", "y"), population="full", tabs=1, environment=True):
 		super().__init__(data, label, population, tabs, environment)
-		self.gdv = GroupedDataVariance(
+		self.gdsd = GroupedDataVariance(
 			self.data,
 			self.label,
 			self.population,
@@ -17,8 +18,12 @@ class GroupedDataSD(StatisticsEquation):
 
 	@property
 	def result(self):
-		return round(sqrt(self.gdv.result), 2)
+		return round(sqrt(self.gdsd.result), 2)
 
 	@property
 	def latex(self):
-		pass
+		output = self.gdsd.latex + f"{new_line() * 2}"
+		output += f"{self.gdsd.prefix}{s(self.gdsd.result)} {new_line()}"
+		output += f"{self.gdsd.prefix}{self.result} \n"
+
+		return output
