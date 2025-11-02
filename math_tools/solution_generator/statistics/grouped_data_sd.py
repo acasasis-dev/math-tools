dcb5af2ad7e5bd3f77833f0b06575068a04ca9f9
acasasis-dev <angelito.casasis@gmail.com>
2025-11-02 17:12:04 +0800

    Get the grouped data variance for solving grouped data sd

--- math_tools/solution_generator/statistics/grouped_data_sd.py
+++ math_tools/solution_generator/statistics/grouped_data_sd.py
@@ -1,9 +1,17 @@
 from .lib import StatisticsEquation
+from .grouped_data_variance import GroupedDataVariance
 
 
 class GroupedDataSD(StatisticsEquation):
 	def __init__(self, data, label=None, population="full", tabs=1, environment=True):
 		super().__init__(data, label, population, tabs, environment)
+		self.gdv = GroupedDataVariance(
+			self.data,
+			self.label,
+			self.population,
+			self.tabs,
+			False
+		)
 
 	@property
 	def result(self):
