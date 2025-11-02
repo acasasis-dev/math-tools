88b6c42672462902dec072af314f6409b565a6e1
acasasis-dev <angelito.casasis@gmail.com>
2025-11-02 17:10:39 +0800

    Init grouped data sd

---
+++ math_tools/solution_generator/statistics/grouped_data_sd.py
@@ -0,0 +1,14 @@
+from .lib import StatisticsEquation
+
+
+class GroupedDataSD(StatisticsEquation):
+	def __init__(self, data, label=None, population="full", tabs=1, environment=True):
+		super().__init__(data, label, population, tabs, environment)
+
+	@property
+	def result(self):
+		pass
+
+	@property
+	def latex(self):
+		pass
