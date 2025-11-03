2f534046eb4f54c7793393c7c27cc8e24bf2d781
acasasis-dev <angelito.casasis@gmail.com>
2025-11-03 14:54:14 +0800

    Added rendering of label on mean

--- math_tools/solution_generator/statistics/mean.py
+++ math_tools/solution_generator/statistics/mean.py
@@ -1,5 +1,5 @@
 from statistics import mean as m
-from math_tools.tools.latex import frac, new_line, mu, x_bar
+from math_tools.tools.latex import frac, new_line, get_mean_symbol
 from .lib import StatisticsEquation
 
 
@@ -16,8 +16,8 @@ class Mean(StatisticsEquation):
 		points_mean = self.result
 		points_stringified = " + ".join(list(map(str, self.data)))
 		points_len = len(self.data)
-		prefix = mu(self.label) if self.population == "full" else x_bar(self.label)
-		output = f"{'\t' * self.tabs}{prefix} = {frac(points_stringified, points_len)} = {frac(sum(self.data), points_len)} = {points_mean} {new_line()}"
+		prefix = get_mean_symbol(self.population, self.label)
+		output = f"{'\t' * self.tabs}{prefix}{frac(points_stringified, points_len)} = {frac(sum(self.data), points_len)} = {points_mean} {new_line()}"
 		if self.environment:
 			output = (
 				"\\begin{gather*} \n"
