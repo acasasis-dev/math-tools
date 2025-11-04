006bc2fd2fd1c1d95c94b62bd19689f4e4fb4158
acasasis-dev <angelito.casasis@gmail.com>
2025-11-04 19:13:45 +0800

    Implemented latex if a list is being provided in all outcomes arg

--- math_tools/solution_generator/probability/simple_probability.py
+++ math_tools/solution_generator/probability/simple_probability.py
@@ -9,11 +9,20 @@ class SimpleProbability(ProbabilityEquation):
 
 	@property
 	def result(self):
-		return round(self.fav_outcomes / self.all_outcomes, 2) * 100
+		if type(self.all_outcomes) in [list, tuple]:
+			all_outcomes = sum(self.all_outcomes)
+		else:
+			all_outcomes = self.all_outcomes
+		return round(self.fav_outcomes / all_outcomes, 2) * 100
 
 	@property
 	def latex(self):
 		prefix = f"{"\t" * self.tabs}P({text(self.label)}) = "
-		self.output = f"{prefix}{frac(self.fav_outcomes, self.all_outcomes)} = {round(self.fav_outcomes / self.all_outcomes, 2)} = {self.result}\\% {new_line()}"
-
+		if type(self.all_outcomes) in [list, tuple]:
+			mid = f"{frac(self.fav_outcomes, " + ".join(list(map(str, self.all_outcomes))))} = {frac(self.fav_outcomes, round(sum(self.all_outcomes), 2))}"
+			suffix = f" = {round(self.fav_outcomes / sum(self.all_outcomes), 2)} = {self.result}\\% {new_line()}"
+		else:
+			mid = frac(self.fav_outcomes, self.all_outcomes)
+			suffix = f" = {round(self.fav_outcomes / self.all_outcomes, 2)} = {self.result}\\% {new_line()}"
+		self.output = f"{prefix}{mid}{suffix}"
 		return super(ProbabilityEquation, self).latex
