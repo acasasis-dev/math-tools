9aa418f0f67846ccf5517f8dd80695c22d91a746
acasasis-dev <angelito.casasis@gmail.com>
2025-11-04 18:03:50 +0800

    Init Probability Equation base class

---
+++ math_tools/solution_generator/probability/lib.py
@@ -0,0 +1,8 @@
+from math_tools.common.equation import Equation
+
+
+class ProbabilityEquation(Equation):
+	def __init__(self, fav_outcomes, all_outcomes, given_outcomes=None):
+		self.fav_outcomes = fav_outcomes
+		self.all_outcomes = all_outcomes
+		self.given_outcomes = given_outcomes
