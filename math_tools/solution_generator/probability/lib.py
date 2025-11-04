b7cfc3b2c8c0578a529ed8b3c879ba3ec46a8cd0
acasasis-dev <angelito.casasis@gmail.com>
2025-11-04 18:44:18 +0800

    Added label and tabs in base class ProbabilityEquation

--- math_tools/solution_generator/probability/lib.py
+++ math_tools/solution_generator/probability/lib.py
@@ -2,8 +2,10 @@ from math_tools.common.equation import Equation
 
 
 class ProbabilityEquation(Equation):
-	def __init__(self, fav_outcomes, all_outcomes, given_outcomes=None, environment=True):
+	def __init__(self, fav_outcomes, all_outcomes, given_outcomes=None, label=None, tabs=1, environment=True):
 		self.fav_outcomes = fav_outcomes
 		self.all_outcomes = all_outcomes
 		self.given_outcomes = given_outcomes
+		self.label = label
+		self.tabs = tabs
 		self.environment = environment
