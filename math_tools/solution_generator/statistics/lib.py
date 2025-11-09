af256938a9550b49b70db95524efeb61095c1376
acasasis-dev <angelito.casasis@gmail.com>
2025-11-09 11:25:13 +0800

    Removed the init of label, tabs and environment in statistics equation

--- math_tools/solution_generator/statistics/lib.py
+++ math_tools/solution_generator/statistics/lib.py
@@ -3,9 +3,7 @@ from math_tools.common.equation import Equation
 
 class StatisticsEquation(Equation):
 	def __init__(self, data, label=None, population="full", tabs=1, environment=True):
+		super().__init__(label, tabs, environment)
 		self.data = data
-		self.label = label
 		self.population = population
-		self.tabs = tabs
-		self.environment = environment
 		
