a2a3b6f2a5d6f1214ecbb2b227b6f97047fa6f74
acasasis-dev <angelito.casasis@gmail.com>
2025-11-09 11:25:54 +0800

    Removed the init of label, tabs and environment in probability equation

--- math_tools/solution_generator/probability/lib.py
+++ math_tools/solution_generator/probability/lib.py
@@ -3,9 +3,7 @@ from math_tools.common.equation import Equation
 
 class ProbabilityEquation(Equation):
 	def __init__(self, fav_outcomes, all_outcomes, given_outcomes=None, label=None, tabs=1, environment=True):
+		super().__init__(label, tabs, environment)
 		self.fav_outcomes = fav_outcomes
 		self.all_outcomes = all_outcomes
-		self.given_outcomes = given_outcomes
-		self.label = label
-		self.tabs = tabs
-		self.environment = environment
+		self.given_outcomes = given_outcomes		
