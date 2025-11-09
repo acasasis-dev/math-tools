521f48d9107094ab07dd051190be33580d60d177
acasasis-dev <angelito.casasis@gmail.com>
2025-11-09 20:25:45 +0800

    Implemented result of addition rule

--- math_tools/solution_generator/probability/addition_rule.py
+++ math_tools/solution_generator/probability/addition_rule.py
@@ -2,14 +2,15 @@ from math_tools.common.equation import Equation
 
 
 class AdditionRule(Equation):
-	def __init__(self, prob_a, prob_b, prob_a_n_b, label, tabs, environment):
+	def __init__(self, prob_a, prob_b, prob_a_n_b, all_outcomes, label, tabs, environment):
 		super().__init__(label, tabs, environment)
 		self.prob_a = prob_a
 		self.prob_b = prob_b
 		self.prob_a_n_b = prob_a_n_b
+		self.all_outcomes = all_outcomes
 
 	def result(self):
-		pass
+		return round(((self.prob_a + self.prob_b) - self.prob_a_n_b) / self.all_outcomes, 2)
 
 	def latex(self):
 		pass
